d547c3f3517b6a8c72bdbfd57c38c9152be4a345
BlueSkyFish <blueskyfish@users.noreply.github.com>
2020-07-07 17:17:44 +0200

    Add view pages and routing plan

---
+++ frontend-app/src/app/login/login.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginViewComponent } from './view/login-view.component';
+
+
+/**
+ * Login module
+ */
+@NgModule({
+  declarations: [
+    LoginViewComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+})
+export class BikeLoginModule { }
---
+++ frontend-app/src/app/register/register.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterViewComponent } from './view/register-view.component';
+
+
+/**
+ * Register module:
+ */
+@NgModule({
+  declarations: [
+    RegisterViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BikeRegisterModule { }
