b3eb192d64dea3247a80402429d60267267221ed
BlueSkyFish <blueskyfish@users.noreply.github.com>
2020-07-07 16:42:51 +0200

    Improve routing

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -4,7 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      scrollPositionRestoration: 'enabled',
+      paramsInheritanceStrategy: 'always',
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
