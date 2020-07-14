3819422b50f27278030276aa75d4b6098558502f
Blueskyfish <blueskyfish@blueskyfish.de>
2020-07-14 15:31:37 +0200

    dialog: add dialog service

--- frontend-app/src/app/common/common.module.ts
+++ frontend-app/src/app/common/common.module.ts
@@ -2,6 +2,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { DialogConfig } from './dialog';
 import { BACKEND_CONFIG, BackendService } from './service/backend.service';
 import { STORAGE_TOKEN } from './service/storage.service';
 
@@ -14,6 +15,22 @@ export interface BikeCommonConfig {
    * The backend api url
    */
   backendApi: string;
+
+  /** disable to click on backdrop is cancel / close the dialog */
+  disableClose?: boolean;
+
+  /** The width of the overlay panel. If a number is provided, pixel units are assumed. */
+  width?: number | string;
+  /** The height of the overlay panel. If a number is provided, pixel units are assumed. */
+  height?: number | string;
+  /** The min-width of the overlay panel. If a number is provided, pixel units are assumed. */
+  minWidth?: number | string;
+  /** The min-height of the overlay panel. If a number is provided, pixel units are assumed. */
+  minHeight?: number | string;
+  /** The max-width of the overlay panel. If a number is provided, pixel units are assumed. */
+  maxWidth?: number | string;
+  /** The max-height of the overlay panel. If a number is provided, pixel units are assumed. */
+  maxHeight?: number | string;
 }
 
 @NgModule({
@@ -41,6 +58,18 @@ export class BikeCommonModule {
           provide: HTTP_INTERCEPTORS,
           useExisting: BackendService,
           multi: true,
+        },
+        {
+          provide: DialogConfig,
+          useValue: new DialogConfig({
+            disableClose: config.disableClose,
+            width: config.width,
+            height: config.height,
+            minWidth: config.minWidth,
+            minHeight: config.minHeight,
+            maxWidth: config.maxWidth,
+            maxHeight: config.maxHeight,
+          }),
         }
       ]
     };
